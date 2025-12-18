e4c4c44842bb7105fab3fdda3a5cc4e3ba2d610c
penguinIndustriesCo <edward@darrall.com>
2025-12-18 18:34:32 +1100

    made card pulling system

--- BlackJack.py
+++ BlackJack.py
@@ -1,9 +1,17 @@
 from random import randint
 
 card = None
-
+cardsPulled = []
 
 def drawcard():
     card = randint(1,54)
-    print (card)
-
+    if card in cardsPulled:
+        print("Same card, re-pulling")
+        drawcard()  
+    else:
+        print (card) 
+        cardsPulled.append(card)
+    
+drawcard()
+drawcard()
+print(cardsPulled)
